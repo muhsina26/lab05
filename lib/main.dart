6a8fa8a6065a342e5deba4577eb3ffd7d3977e8b
muhsina26 <tasnim15-5763@diu.edu.bd>
2025-02-18 10:03:05 +0600

    init

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,19 @@ class LabClass05 extends StatelessWidget {
         appBar: AppBar(
           title: Text("Basic Flutter UI 02"),
         ),
-        body: Text(
-          "Basic Flutter UI 02",
-          style: TextStyle(
-            fontSize: 30,
-          ),
-        ),
+        body: Row(children: [
+          Icon(Icons.shop,
+          size:50,
+          color: Colors.red,),
+          Text("This",style: TextStyle(color: Colors.red,
+          fontSize:20),)
+          ,Text(" is",style: TextStyle(color: Colors.blue,
+          fontSize:50),),
+          Text(" Flutter",style: TextStyle(color: Colors.green,
+          fontSize:25),)
+
+        ],)
       ),
     );
   }
-}
+}                          
