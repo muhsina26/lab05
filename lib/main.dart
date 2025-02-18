72be10a95f59d41385896c0da0f4ae34e5fc3bad
muhsina26 <tasnim15-5763@diu.edu.bd>
2025-02-18 11:12:30 +0600

    scrollerview

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ void main() {
   runApp(const LabClass05());
 }
 
+/*overflow Handling
+1. SingleChilScrorll View
+2.Expanded
+3.Flexible
+*/
+
 class LabClass05 extends StatelessWidget {
   const LabClass05({super.key});
 
@@ -11,44 +17,30 @@ class LabClass05 extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      title: "TheFirstApp",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("TheFirstApp"),
-        ),
-        body:
-        
-         Container(
-          height: double.infinity,
-           child: Row(mainAxisAlignment:MainAxisAlignment.spaceEvenly, children: [
-           
-            Icon(Icons.star,
-            color: const Color.fromARGB(255, 154, 151, 151),
-            size: 50,
+        title: "TheFirstApp",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("TheFirstApp"),
+          ),
+          body: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Container(
+              height: 600,
+              // width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: List.generate(
+                  100,
+                  (index) => Icon(
+                    Icons.star,
+                    color: const Color.fromARGB(255, 154, 151, 151),
+                    size: index.toDouble(),
+                  ),
+                ),
+              ),
             ),
-            Icon(Icons.star,
-            color: const Color.fromARGB(255, 139, 136, 136),
-            size: 50,
-            ),
-            Icon(Icons.star,
-            color: const Color.fromARGB(255, 139, 136, 136),
-            size: 50,
-            ),
-            Icon(Icons.star,
-            color: const Color.fromARGB(255, 139, 136, 136),
-            size: 50,
-            ),
-            Icon(Icons.star_border,
-            color: const Color.fromARGB(255, 139, 136, 136),
-            size: 50,
-            ),
-           
-            
-           
-                   ],),
-         )
-        
-      ),
-    );
+          ),
+        ));
   }
-}                          
+}
